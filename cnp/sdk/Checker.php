e8596b8c6fbc25382bb860d185d150878c279539
Joseph Gravelle <jgravell@jgravell-vm2.dwi.litle.com>
2020-12-04 09:26:05 -0500

    upgrading to SchemaCombined_v12.18 from v12.17

--- cnp/sdk/Checker.php
+++ cnp/sdk/Checker.php
@@ -36,7 +36,7 @@ class Checker
     public static function validateXML($request){
         $xml = new DOMDocument();
         $xml->loadXML($request);
-        $filepath = __DIR__ . "/schema/SchemaCombined_v12.17.xsd";
+        $filepath = __DIR__ . "/schema/SchemaCombined_v12.18.xsd";
         $result =  $xml->schemaValidate( $filepath);
 
         if(!$result)
